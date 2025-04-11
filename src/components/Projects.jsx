6493da88a67c579353282e12cc62a0e8488e3aca
medaminhm <104175994+medaminhm@users.noreply.github.com>
2025-04-11 22:47:11 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,14 +5,14 @@ const projects = [
     name: "Indar Deco",
     description:
       "Indar Deco est une plateforme de v-commerce spécialisée dans les produits de mobilier, intégrant des technologies de réalité augmentée (AR) et de réalité virtuelle (VR) pour offrir une expérience d'achat immersive et innovante. Grâce à ces technologies, les utilisateurs peuvent visualiser les produits dans leur environnement via AR et explorer un showroom virtuel en VR, le tout en ligne.",
-    image: "/assets/images/1726764094685-portrait.png", // Remplacer par le chemin réel de l'image
+    image: "src/assets/images/1726764094685-portrait.png", // Remplacer par le chemin réel de l'image
     link: "#", // Mettre le lien réel du projet si nécessaire
   },
 ];
 
 export default function Projects() {
   return (
-    <section id="projects" className="py-12 bg-gray-100">
+    <section id="projets" className="py-12 bg-gray-100">
       <div className="container mx-auto px-6">
         <motion.h2
           className="text-3xl font-bold text-[#284F75] text-center mb-12 font-[orbitron]"
@@ -40,7 +40,8 @@ export default function Projects() {
                 </p>
                 {project.link && (
                   <a
-                    href={project.link}
+                    // href="/indar-deco-details"
+                     href="#"
                     className="mt-4 inline-block text-blue-500 hover:underline"
                   >
                     En savoir plus →
